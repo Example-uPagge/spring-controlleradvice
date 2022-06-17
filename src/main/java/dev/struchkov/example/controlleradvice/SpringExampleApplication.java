1f3e8b75b333111b019dea0045dab268f1fa3904
Struchkov Mark <mark@struchkov.dev>
2022-06-17 16:28:02 +0300

    InitCommit Two

---
+++ src/main/java/dev/struchkov/example/controlleradvice/SpringExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.struchkov.example.controlleradvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleApplication.class, args);
+    }
+
+}
